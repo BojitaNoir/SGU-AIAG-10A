3bc402e75f050b51f3b5696bc77d86b011792e21
BojitaNoir <20213tn094@utez.edu.mx>
2025-11-02 20:25:19 -0600

    referencia eliminada

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'sweetalert2/dist/sweetalert2.min.css'
 import App from './App.jsx'
-import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
